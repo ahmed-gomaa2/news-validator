40217a5cc7354d1dfa58a077ae0d583c4943f92d
ahmed-gomaa2 <engahmedgomaa97@gmail.com>
2021-03-29 12:02:44 +0200

    remove the second test

--- __test__/testValidateURL.spec.js
+++ __test__/testValidateURL.spec.js
@@ -1,16 +1,13 @@
-import { handleSubmit } from "../src/client";
+import { validateURL } from "../src/client/js/validateURL";
 
 describe("Testing the validateURL functionality", () => {
     // The test() function has two arguments - a string description, and an actual test as a callback function.
-    test("Testing the validateURL() function", (done) => {
+    test("Testing the validateURL() function", () => {
         // Define the input for the function, if any, in the form of variables/array
 
         // Define the expected output, if any, in the form of variables/array
         // The expect() function, in combination with a Jest matcher, is used to check if the function produces the expected output
         // The general syntax is `expect(myFunction(arg1, arg2, ...)).toEqual(expectedValue);`, where `toEqual()` is a matcher
-        handleSubmit(() => {
-            expect(handleSubmit).toBeDefined();
-            done();
-        });
+        expect(validateURL).toBeDefined();
     });
 });
